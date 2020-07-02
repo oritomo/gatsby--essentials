e98df127cdd7bc057d32843d7d3413f9a821db0d
Tomoya Orito <tomoyaorito@TomoyanoMacBook-Air.local>
2020-07-02 15:12:33 +0900

    4章終わり

--- src/components/layout.js
+++ src/components/layout.js
@@ -2,7 +2,11 @@ import React from 'react';
 import Header from "../components/header";
 import Footer from "../components/footer";
 
-import "./layout.css";
+import "./layout.css"
+
+import "@fortawesome/fontawesome-svg-core/styles.css"
+import { config } from "@fortawesome/fontawesome-svg-core" 
+config.autoAddCss = false
 
 export default ( {children} ) => (
     <div>
